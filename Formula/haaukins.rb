e9fb27d63c582cdbaeb23be993938faa49dc1473
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-11 13:00:13 +0100

    Brew formula update for haaukins version 2.2.5

--- Formula/haaukins.rb
+++ Formula/haaukins.rb
@@ -2,16 +2,16 @@
 class Haaukins < Formula
   desc "Haaukins CLI"
   homepage "https://github.com/aau-network-security/haaukins/"
-  version "2.2.4"
+  version "2.2.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/aau-network-security/haaukins/releases/download/2.2.4/haaukins_cli_2.2.4_macOS_64-bit.zip"
-    sha256 "93f3f6c5723add50618b16b2e9dd4138b266ede6a858072b0a0d3ef59752afaf"
+    url "https://github.com/aau-network-security/haaukins/releases/download/2.2.5/haaukins_cli_2.2.5_macOS_64-bit.zip"
+    sha256 "a9e56949f5c10d8fad11f538b506a44f33c6efc05fea71ca5f9cd90a339b5847"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/aau-network-security/haaukins/releases/download/2.2.4/haaukins_cli_2.2.4_linux_64-bit.zip"
-      sha256 "d0e167864c97a2322ef04cd5c0795be133f6e9f2ada393a68f5ac6221f8f86e8"
+      url "https://github.com/aau-network-security/haaukins/releases/download/2.2.5/haaukins_cli_2.2.5_linux_64-bit.zip"
+      sha256 "d274ae4c8af315480285cde28b528d97d167a3245a748dbcf4bb8e073fd8c7ad"
     end
   end
 
